bc51bb5379f71ac629b6609707ccaf1d15e527a9
Christian Sattler <sattler.christian@gmail.com>
2021-01-27 15:14:21 +0100

    Initial commit

---
+++ Autocomplete.java
@@ -0,0 +1,34 @@
+
+public class Autocomplete {
+    private Term[] dictionary;
+
+    // Initializes the dictionary from the given array of terms.
+    public Autocomplete(Term[] dictionary) {
+        this.dictionary = dictionary;
+        sortDictionary();
+    }
+
+    // Sorts the dictionary in *case-insensitive* lexicographic order.
+    // Complexity: O(N log N) where N is the number of dictionary terms
+    private void sortDictionary() {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    // Returns all terms that start with the given prefix, in descending order of weight.
+    // Precondition: the internal dictionary is in lexicographic order.
+    // Complexity: O(log N + M log M) where M is the number of matching terms
+    public Term[] allMatches(String prefix) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+    // Returns the number of terms that start with the given prefix.
+    // Precondition: the internal dictionary is in lexicographic order.
+    // Complexity: O(log N) where N is the number of dictionary terms
+    public int numberOfMatches(String prefix) {
+        // TODO
+        throw new UnsupportedOperationException();
+    }
+
+}
